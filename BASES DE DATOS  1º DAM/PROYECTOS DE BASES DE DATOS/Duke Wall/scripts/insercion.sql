4f83ceb121ccdfe3669ce882fb494e78d6d0fa15
DukeW <wallduke@gmail.com>
2025-04-12 21:43:56 +0100

    Update insercion.sql

--- BASES DE DATOS  1º DAM/PROYECTOS DE BASES DE DATOS/Duke Wall/scripts/insercion.sql
+++ BASES DE DATOS  1º DAM/PROYECTOS DE BASES DE DATOS/Duke Wall/scripts/insercion.sql
@@ -3,14 +3,14 @@
 # Aqui añadiremos los datos y tambien podremos ver si esta funcionando correctamente el "SELECT"
 
 #PROVEEDOR
-INSERT INTO proveedor (cif, correo, numeroDeContacto, direccionProveedor)
-VALUES 
+INSERT INTO globalmart.proveedor (cif, correo, numeroDeContacto, direccionProveedor) 
+VALUE
 ('B98742356', 'dwall@globalmart.com', 697654291, 'C. Aguere, 17, 38005 Santa Cruz de Tenerife');
 
-SELECT * FROM proveedor;
+SELECT * FROM globalmart.proveedor;
 
 # CLIENTE 
-INSERT INTO cliente (tipoCliente, nombreCliente, preferenciaDePago, telefonoCliente, correoCliente)
+INSERT INTO globalmart.cliente (tipoCliente, nombreCliente, preferenciaDePago, telefonoCliente, correoCliente)
 VALUES
 ('particular', 'Juan Pérez', 'Visa', 600123456, 'juan@mail.com'),
 ('empresa', 'TechCorp S.L.', 'MasterCard', 911223344, 'contacto@techcorp.com'),
@@ -19,18 +19,45 @@ VALUES
 ('empresa', 'HelmetCleaner', 'Visa', 922654378, 'helms@criscleansit.com'), 
 ('particular', 'Jose Manuel', 'MasterCard', 675439889, null);
 
-SELECT * FROM cliente;
+
+SELECT * FROM globalmart.cliente;
+
 
 # Luego el PRODUCTOS
-INSERT INTO producto(idProducto, idProveedor, nombreProducto, fechaCaducidad, stock, precioProducto,idPedido)
+INSERT INTO globalmart.producto(idProveedor,nombreProducto, fecchaCaducidad, stock, precioProducto)
 VALUES
-(DEFAULT, 1, "Mochila de viaje", null, 32, 49.99, null),
-(DEFAULT, 1, "Fertilizante plantas aromáticas", "12-09-2027", 123, 6.65, null),
-(DEFAULT, 1, "Tabla de surf", null, 5, 120, null),
-(DEFAULT, 1, "Llavero de StarWars", null, 0, 5, null),
-(DEFAULT, 1, "Stannley Cup", null, 436, 33.79),
-(DEFAULT, 1, "Vela aroma Oakwood", "04-12-2030", 12, 12.50, null),
-(DEFAULT, 1, "Tira de luces LED", null, 178, 9.98, null),
-(DEFAULT, 1, "Juego de mesa CATAN", null, 2, 80, null);
+( 1, 'Mochila de viaje', null, 32, 49.99),
+( 1, 'Fertilizante plantas aromáticas', '2027-09-17', 123, 6.65),
+( 1, 'Tabla de surf', null, 5, 120),
+( 1, 'Llavero de StarWars', null, 0, 5),
+( 1, 'Stannley Cup', null, 436, 33.79),
+( 1, 'Vela aroma Oakwood', '2030-12-04', 12, 12.50),
+( 1, 'Tira de luces LED', null, 178, 9.98),
+( 1, 'Juego de mesa CATAN', null, 2, 80);
 
-SELECT * FROM producto;
+SELECT * FROM globalmart.producto;
+
+
+# El PEDIDO
+
+INSERT INTO globalmart.pedido (fechaDePedido, direccionEntrega, idCliente)
+VALUE
+('2025-02-04', 'Calle Adan Martin, 13, 38005', 3),
+('2025-01-05', 'Calle del Agua, 5, planta 2, puerta h', 1),
+('2025-04-01', 'Avenda Mencey, 2, planta 1, puetra 3', 6),
+('2024-12-29', 'Calle del Agua, 5, planta 2, puerta h', 1),
+('2025-04-15', 'Avd. Los Majuelos, 4', 2),
+('2025-03-21', 'Avenida Castellon, planta 6, puerta 15', 4);
+
+SELECT * FROM globalmart.pedido;
+
+INSERT INTO globalmart.detallepedido(idProducto, cantidad, estadoDePedido, idPedido)
+VALUE 
+(1, 1, 'preparando', 1),
+(3, 1, 'entregado', 4),
+(2, 10,'preparando', 5),
+(6, 3, 'reparto', 3),
+(8, 1, 'entregado', 2),
+(7, 6, 'entregado', 6);
+
+SELECT * FROM globalmart.detallepedido;

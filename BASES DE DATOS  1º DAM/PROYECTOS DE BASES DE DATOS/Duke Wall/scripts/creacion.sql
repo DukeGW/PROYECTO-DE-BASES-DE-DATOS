fa2614e1faedd625e57236f96174a8ca8bf9f3ae
DukeW <wallduke@gmail.com>
2025-04-18 16:50:55 +0100

    Update creacion.sql

--- BASES DE DATOS  1º DAM/PROYECTOS DE BASES DE DATOS/Duke Wall/scripts/creacion.sql
+++ BASES DE DATOS  1º DAM/PROYECTOS DE BASES DE DATOS/Duke Wall/scripts/creacion.sql
@@ -1,9 +1,9 @@
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 CREATE SCHEMA IF NOT EXISTS `Globalmart` DEFAULT CHARACTER SET utf8 ;
+USE `Globalmart` ;
 
 CREATE TABLE IF NOT EXISTS `Globalmart`.`Proveedor` (
   `idProveedor` INT(11) NOT NULL AUTO_INCREMENT,
@@ -14,6 +14,36 @@ CREATE TABLE IF NOT EXISTS `Globalmart`.`Proveedor` (
   PRIMARY KEY (`idProveedor`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `Globalmart`.`Cliente` (
+  `idCliente` INT(11) NOT NULL AUTO_INCREMENT,
+  `tipoCliente` ENUM('particular', 'empresa') NOT NULL,
+  `nombreCliente` VARCHAR(45) NOT NULL,
+  `preferenciaDePago` ENUM('MasterCard', 'Visa') NULL DEFAULT NULL,
+  `telefonoCliente` INT(11) NULL DEFAULT NULL,
+  `correoCliente` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`idCliente`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `Globalmart`.`Pedido` (
+  `idPedido` INT(11) NOT NULL AUTO_INCREMENT,
+  `idProducto` INT(11) NOT NULL,
+  `fechaDePedido` DATE NOT NULL,
+  `direccionEntrega` MEDIUMTEXT NOT NULL,
+  `idCliente` INT(11) NOT NULL,
+  PRIMARY KEY (`idPedido`),
+  INDEX `fk_Pedido_Cliente1_idx` (`idCliente` ASC) VISIBLE,
+  CONSTRAINT `fk_Pedido_Cliente1`
+    FOREIGN KEY (`idCliente`)
+    REFERENCES `Globalmart`.`Cliente` (`idCliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
 
 CREATE TABLE IF NOT EXISTS `Globalmart`.`Producto` (
   `idProducto` INT(11) NOT NULL AUTO_INCREMENT,
@@ -39,32 +69,6 @@ CREATE TABLE IF NOT EXISTS `Globalmart`.`Producto` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS `Globalmart`.`Cliente` (
-  `idCliente` INT(11) NOT NULL AUTO_INCREMENT,
-  `tipoCliente` ENUM('particular', 'empresa') NOT NULL,
-  `nombreCliente` VARCHAR(45) NOT NULL,
-  `preferenciaDePago` ENUM('MasterCard', 'Visa') NULL DEFAULT NULL,
-  `telefonoCliente` INT(11) NULL DEFAULT NULL,
-  `correoCliente` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`idCliente`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS `Globalmart`.`Pedido` (
-  `idPedido` INT(11) NOT NULL AUTO_INCREMENT,
-  `idProducto` INT(11) NOT NULL,
-  `fechaDePedido` DATE NOT NULL,
-  `direccionEntrega` MEDIUMTEXT NOT NULL,
-  `idCliente` INT(11) NOT NULL,
-  PRIMARY KEY (`idPedido`),
-  INDEX `fk_Pedido_Cliente1_idx` (`idCliente` ASC) VISIBLE,
-  CONSTRAINT `fk_Pedido_Cliente1`
-    FOREIGN KEY (`idCliente`)
-    REFERENCES `Globalmart`.`Cliente` (`idCliente`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `Globalmart`.`DetallePedido` (
   `idDetallePedido` INT(11) NOT NULL AUTO_INCREMENT,
